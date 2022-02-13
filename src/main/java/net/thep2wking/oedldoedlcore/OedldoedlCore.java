67041863d2c5a6fac5c6716272d8302f127f41e2
TheP2WKing <78441179+TheP2WKing@users.noreply.github.com>
2022-02-13 22:12:57 +0100

    Add files via upload

--- src/main/java/net/thep2wking/oedldoedlcore/OedldoedlCore.java
+++ src/main/java/net/thep2wking/oedldoedlcore/OedldoedlCore.java
@@ -6,15 +6,12 @@ import org.apache.logging.log4j.Logger;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.thep2wking.oedldoedlcore.config.CoreConfig;
-import net.thep2wking.oedldoedlcore.config.CoreConfigLogging;
 import net.thep2wking.oedldoedlcore.init.ModItems;
 
 @Mod(OedldoedlCore.MODID)
@@ -22,7 +19,7 @@ public class OedldoedlCore
 {
 	public static final String MODID = "oedldoedlcore";
 	public static final String NAME = "Oedldoedl Core";
-	public static final String VERSION = "1.16.5-3.1.0";
+	public static final String VERSION = "1.16.5-3.5.0";
 	
 	public static OedldoedlCore INSTANCE;
 	
@@ -30,10 +27,14 @@ public class OedldoedlCore
 
     public OedldoedlCore() 
     {
+    	LOGGER.info("Hello, World!");
+    	
+    	CoreConfig.init();
+    	
+    	if (CoreConfig.general_logging.get()) {LOGGER.info("Registerd Config for" + MODID + "!");}
+    	
     	IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-    	
-    	ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CoreConfig.CONFIG);
-    	
+
     	ModItems.register(eventBus);
     	
     	eventBus.addListener(this::onCommonSetup);
@@ -45,35 +46,28 @@ public class OedldoedlCore
 
     private void onCommonSetup(final FMLCommonSetupEvent event)
     {
-    	if (CoreConfigLogging.general.get())
-    	{
-    		LOGGER.info("Common Setup Registerd!");
-    	}
+    	if (CoreConfig.general_logging.get()) {LOGGER.info("Registerd Common Setup for" + MODID + "!");}
     }
 
 	private void onClientSetup(final FMLClientSetupEvent event) 
     {
-    	if (CoreConfigLogging.general.get())
-    	{
-    		LOGGER.info("Client Setup Registerd!");
-    	}
+    	if (CoreConfig.general_logging.get()) {LOGGER.info("Registerd Client Setup for" + MODID + "!");}
     }
 	
-	public void onloadComplete(final FMLLoadCompleteEvent event) 
+	private void onloadComplete(final FMLLoadCompleteEvent event) 
 	{
-    	if (CoreConfigLogging.general.get())
-    	{
-			LOGGER.info(NAME + " " + VERSION + " successfully loaded!");
-    	}
+    	if (CoreConfig.integrations_logging.get() && ModList.get().isLoaded("jei")) {LOGGER.info("Loaded JEI Compat for" + MODID + "!");}
+    	if (CoreConfig.integrations_logging.get() && ModList.get().isLoaded("configured")) {LOGGER.info("Loaded Configured Compat for" + MODID + "!");}
     	
-    	if (CoreConfigLogging.submods.get())
+    	if (CoreConfig.submods_logging.get())
     	{
-			LOGGER.info("Oedldoedl Core is installed: " + ModList.get().isLoaded("oedldoedlcore"));
 			LOGGER.info("Oedldoedl Resources is installed: " + ModList.get().isLoaded("oedldoedlresources"));
 			LOGGER.info("Oedldoedl Gear is installed: " + ModList.get().isLoaded("oedldoedlgear"));
 			LOGGER.info("Oedldoedl Construct is installed: " + ModList.get().isLoaded("oedldoedlconstruct"));
 			LOGGER.info("Oedldoedl Music is installed: " + ModList.get().isLoaded("oedldoedlmusic"));
 			LOGGER.info("Oedldoedl Integration is installed: " + ModList.get().isLoaded("oedldoedlintegration"));
     	}
-	}
+    	
+    	if (CoreConfig.general_logging.get()) {LOGGER.info(NAME + " " + VERSION + " successfully loaded!");}
+	}		
 }
