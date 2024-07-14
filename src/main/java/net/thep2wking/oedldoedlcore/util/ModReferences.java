583460c5f0727f0d47831dbd702b726af38c74a1
TheP2WKing <tdziomba@hotmail.com>
2024-07-14 16:26:00 +0200

    fixed reference and asset issues

--- src/main/java/net/thep2wking/oedldoedlcore/util/ModReferences.java
+++ src/main/java/net/thep2wking/oedldoedlcore/util/ModReferences.java
@@ -12,6 +12,8 @@ public class ModReferences {
 	public static final String BASE_NAME = "Oedldoedl";
 	public static final String MINECRAFT_MODID = "minecraft";
 	public static final String MINECRAFT_NAME = "Minecraft";
+	public static final String FORGE_MODID = "forge";
+	public static final String FORGE_NAME = "Minecraft Forge";
 
 	public static final String MODID_CORE = BASE_MODID + "core";
 	public static final String MODID_RESOURCES = BASE_MODID + "resources";
@@ -34,32 +36,32 @@ public class ModReferences {
 	public static final String NAME_INTEGRATION = BASE_NAME + " " + "Integration";
 
 	// creative tab resource locations
-	public static final ResourceLocation CREATIVE_TAB_LIGHT = new ResourceLocation("minecraft",
+	public static final ResourceLocation CREATIVE_TAB_LIGHT = new ResourceLocation(MINECRAFT_MODID,
 			"textures/gui/container/creative_inventory/tab_items.png");
-	public static final ResourceLocation CREATIVE_TAB_LIGHT_SEARCH = new ResourceLocation("minecraft",
+	public static final ResourceLocation CREATIVE_TAB_LIGHT_SEARCH = new ResourceLocation(MINECRAFT_MODID,
 			"textures/gui/container/creative_inventory/tab_item_search.png");
 
 	public static final ResourceLocation CREATIVE_TAB_DARK = new ResourceLocation(OedldoedlCore.MODID,
 			"textures/gui/container/tab_oedldoedl_base.png");
-	public static final ResourceLocation CREATIVE_TAB_DARK_SEARACH = new ResourceLocation(OedldoedlCore.MODID,
+	public static final ResourceLocation CREATIVE_TAB_DARK_SEARCH = new ResourceLocation(OedldoedlCore.MODID,
 			"textures/gui/container/tab_oedldoedl_search.png");
 
 	// attribute names
-	public static final String ATTRIBUTE_ARMOR = "Oedldoedl Armor Attribute";
-	public static final String ATTRIBUTE_ARMOR_TOUGHNESS = "Oedldoedl Armor Toughness Attribute";
-	public static final String ATTRIBUTE_ATTACK_DAMAGE = "Oedldoedl Attack Damage Attribute";
-	public static final String ATTRIBUTE_ATTACK_KNOCKBACK = "Oedldoedl Attack Knockback Attribute";
-	public static final String ATTRIBUTE_ATTACK_SPEED = "Oedldoedl Attack Speed Attribute";
-	public static final String ATTRIBUTE_FLYING_SPEED = "Oedldoedl Flying Speed Attribute";
-	public static final String ATTRIBUTE_FOLLOW_RANGE = "Oedldoedl Follow Range Attribute";
-	public static final String ATTRIBUTE_HORSE_JUMP_STRENGTH = "Oedldoedl Horse Jump Strength Attribute";
-	public static final String ATTRIBUTE_KNOCKBACK_RESISTANCE = "Oedldoedl Knockback Resistance Attribute";
-	public static final String ATTRIBUTE_LUCK = "Oedldoedl Luck Attribute";
-	public static final String ATTRIBUTE_MAX_HEALTH = "Oedldoedl Max Health Attribute";
-	public static final String ATTRIBUTE_MOVEMENT_SPEED = "Oedldoedl Movement Speed Attribute";
-	public static final String ATTRIBUTE_ZOMBIE_SPAWN_REINFORCEMENTS = "Oedldoedl Zombie Spawn Reinforcements Attribute";
-	public static final String ATTRIBUTE_SWIN_SPEED = "Oedldoedl Swim Speed Attribute";
-	public static final String ATTRIBUTE_NAMETAG_DISTANCE = "Oedldoedl Nametag Distance Attribute";
+	public static final String ATTRIBUTE_ARMOR = BASE_NAME + "Armor Attribute";
+	public static final String ATTRIBUTE_ARMOR_TOUGHNESS = BASE_NAME + "Armor Toughness Attribute";
+	public static final String ATTRIBUTE_ATTACK_DAMAGE = BASE_NAME + "Attack Damage Attribute";
+	public static final String ATTRIBUTE_ATTACK_KNOCKBACK = BASE_NAME + "Attack Knockback Attribute";
+	public static final String ATTRIBUTE_ATTACK_SPEED = BASE_NAME + "Attack Speed Attribute";
+	public static final String ATTRIBUTE_FLYING_SPEED = BASE_NAME + "Flying Speed Attribute";
+	public static final String ATTRIBUTE_FOLLOW_RANGE = BASE_NAME + "Follow Range Attribute";
+	public static final String ATTRIBUTE_HORSE_JUMP_STRENGTH = BASE_NAME + "Horse Jump Strength Attribute";
+	public static final String ATTRIBUTE_KNOCKBACK_RESISTANCE = BASE_NAME + "Knockback Resistance Attribute";
+	public static final String ATTRIBUTE_LUCK = BASE_NAME + "Luck Attribute";
+	public static final String ATTRIBUTE_MAX_HEALTH = BASE_NAME + "Max Health Attribute";
+	public static final String ATTRIBUTE_MOVEMENT_SPEED = BASE_NAME + "Movement Speed Attribute";
+	public static final String ATTRIBUTE_ZOMBIE_SPAWN_REINFORCEMENTS = BASE_NAME + "Zombie Spawn Reinforcements Attribute";
+	public static final String ATTRIBUTE_SWIN_SPEED = BASE_NAME + "Swim Speed Attribute";
+	public static final String ATTRIBUTE_NAMETAG_DISTANCE = BASE_NAME + "Nametag Distance Attribute";
 	public static final String ATTRIBUTE_ENTITY_GRAVITY = "Oedldoedl Entity Gravity Attribute";
-	public static final String ATTRIBUTE_REACH_DISTANCE = "Oedldoedl Reach Distance Attribute";
+	public static final String ATTRIBUTE_REACH_DISTANCE = BASE_NAME + "Reach Distance Attribute";
 }
