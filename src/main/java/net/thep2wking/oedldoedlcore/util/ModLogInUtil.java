6282cd73c03505b57ccaba4fc7a62cff1c5e976c
TheP2WKing <tdziomba@hotmail.com>
2024-08-16 21:36:13 +0200

    added aability fur custom messages and moved translation to lang file

--- src/main/java/net/thep2wking/oedldoedlcore/util/ModLogInUtil.java
+++ src/main/java/net/thep2wking/oedldoedlcore/util/ModLogInUtil.java
@@ -2,12 +2,15 @@ package net.thep2wking.oedldoedlcore.util;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.event.ClickEvent;
 import net.minecraft.util.text.event.ClickEvent.Action;
 import net.minecraft.util.text.event.HoverEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
+import net.thep2wking.oedldoedlcore.OedldoedlCore;
 import net.thep2wking.oedldoedlcore.config.CoreConfig;
 
 /**
@@ -15,28 +18,48 @@ import net.thep2wking.oedldoedlcore.config.CoreConfig;
  */
 public class ModLogInUtil {
 	// join message component
-	public static ITextComponent joinMessageTextComponent(String name, String modid, String version) {
-		String urlString = "https://www.curseforge.com/minecraft/mc-mods/"
-				+ modid.replace(ModReferences.BASE_MODID, ModReferences.BASE_MODID + "-");
+	public static ITextComponent joinMessageTextComponent(String name, String curseForgeId, String version,
+			boolean hasCustomMessage) {
+		String url = new TextComponentTranslation("join." + OedldoedlCore.MODID + ".url").getUnformattedComponentText();
+		String urlString = url + curseForgeId.replace(ModReferences.BASE_MODID, ModReferences.BASE_MODID + "-");
+		String versionInfo = CoreConfig.LOGGING.JOIN_MESSAGES_VERSION_INFO ? " v" + version : "";
 		ITextComponent joinMessage = new TextComponentString(TextFormatting.DARK_GREEN + "[")
-				.appendSibling(new TextComponentString(TextFormatting.GREEN + name + " v" + version))
+				.appendSibling(new TextComponentString(TextFormatting.GREEN + name + versionInfo))
 				.appendSibling(new TextComponentString(TextFormatting.DARK_GREEN + "]"))
-				.appendSibling(new TextComponentString(" installed successfully!"));
+				.appendSibling(new TextComponentString(" "))
+				.appendSibling(hasCustomMessage
+						? new TextComponentTranslation("join." + curseForgeId.replace("-", "") + ".message")
+						: defaultMessage());
 
 		ClickEvent clickEvent = new ClickEvent(Action.OPEN_URL, urlString);
 		joinMessage.getStyle().setClickEvent(clickEvent);
 		HoverEvent hoverEvent = new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-				new TextComponentString("Click to visit the " + name + " CurseForge page..."));
+				new TextComponentTranslation("join." + OedldoedlCore.MODID + ".visit")
+						.appendSibling(new TextComponentString(" " + name + " ").appendSibling(
+								new TextComponentTranslation("join." + OedldoedlCore.MODID + ".curseforge")))
+						.setStyle(new Style().setColor(TextFormatting.GOLD)));
 		joinMessage.getStyle().setHoverEvent(hoverEvent);
-
 		return joinMessage;
 	}
 
+	// default message
+	private static ITextComponent defaultMessage() {
+		return new TextComponentTranslation("join." + OedldoedlCore.MODID + ".default");
+	}
+
 	// add join message
-	public static void addJoinMessage(PlayerLoggedInEvent event, String name, String modid, String version) {
+	public static void addJoinMessage(PlayerLoggedInEvent event, String name, String curseForgeId, String version) {
 		if (event.player instanceof EntityPlayer && !event.player.world.isRemote
 				&& CoreConfig.LOGGING.JOIN_MESSAGES) {
-			event.player.sendMessage(joinMessageTextComponent(name, modid, version));
+			event.player.sendMessage(joinMessageTextComponent(name, curseForgeId, version, false));
+		}
+	}
+
+	public static void addJoinMessage(PlayerLoggedInEvent event, String name, String curseForgeId, String version,
+			boolean hasCustomMessage) {
+		if (event.player instanceof EntityPlayer && !event.player.world.isRemote
+				&& CoreConfig.LOGGING.JOIN_MESSAGES) {
+			event.player.sendMessage(joinMessageTextComponent(name, curseForgeId, version, hasCustomMessage));
 		}
 	}
 }
