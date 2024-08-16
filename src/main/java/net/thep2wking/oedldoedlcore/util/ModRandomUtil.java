a5e0f1631ad048b8baf95fe5c723914947e37f6e
TheP2WKing <tdziomba@hotmail.com>
2024-08-16 21:42:19 +0200

    random util null safety

--- src/main/java/net/thep2wking/oedldoedlcore/util/ModRandomUtil.java
+++ src/main/java/net/thep2wking/oedldoedlcore/util/ModRandomUtil.java
@@ -7,8 +7,21 @@ import java.util.Random;
  * @author TheP2WKing
  */
 public class ModRandomUtil {
+	public static final Random RANDOM = new Random();
+
 	// select random element from a list
 	public static <T> T selectRandom(Random random, List<T> list) {
+		if (list == null || list.isEmpty()) {
+			return null;
+		}
 		return list.get(random.nextInt(list.size()));
+	}
+	
+	public static <T> T selectRandom(List<T> list) {
+		if (list == null || list.isEmpty()) {
+			return null;
+		}
+		Random newRandom = new Random();
+		return list.get(newRandom.nextInt(list.size()));
 	}
 }
