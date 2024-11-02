7beb4bd8d343dff3866f0c1d84e5c0ef54dbaf79
TheP2WKing <tdziomba@hotmail.com>
2024-11-02 13:03:25 +0100

    added top entity provider

---
+++ src/main/java/net/thep2wking/oedldoedlcore/integration/top/ITOPEntityInfoProvider.java
@@ -0,0 +1,13 @@
+package net.thep2wking.oedldoedlcore.integration.top;
+
+import mcjty.theoneprobe.api.IProbeHitEntityData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.ProbeMode;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+public interface ITOPEntityInfoProvider {
+	void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, Entity entity,
+			IProbeHitEntityData data);
+}
--- src/main/java/net/thep2wking/oedldoedlcore/integration/top/TOPCompatibility.java
+++ src/main/java/net/thep2wking/oedldoedlcore/integration/top/TOPCompatibility.java
@@ -5,11 +5,14 @@ import java.util.function.Function;
 import javax.annotation.Nullable;
 
 import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeHitEntityData;
 import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.IProbeInfoEntityProvider;
 import mcjty.theoneprobe.api.IProbeInfoProvider;
 import mcjty.theoneprobe.api.ITheOneProbe;
 import mcjty.theoneprobe.api.ProbeMode;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
@@ -43,9 +46,26 @@ public class TOPCompatibility {
                 @Override
                 public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world,
                         IBlockState blockState, IProbeHitData data) {
-                    if (blockState.getBlock() instanceof ITOPInfoProvider && CoreConfig.INTEGRATION.THEONEPROBE.TOP_INTEGRATION) {
+                    if (blockState.getBlock() instanceof ITOPInfoProvider && blockState != null
+                            && CoreConfig.INTEGRATION.THEONEPROBE.TOP_INTEGRATION) {
                         ITOPInfoProvider provider = (ITOPInfoProvider) blockState.getBlock();
                         provider.addProbeInfo(mode, probeInfo, player, world, blockState, data);
+                    }
+                }
+            });
+            probe.registerEntityProvider(new IProbeInfoEntityProvider() {
+                @Override
+                public String getID() {
+                    return ModReferences.BASE_MODID + ":" + "default";
+                }
+
+                @Override
+                public void addProbeEntityInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world,
+                        Entity entity, IProbeHitEntityData data) {
+                    if (entity instanceof ITOPEntityInfoProvider && entity != null
+                            && CoreConfig.INTEGRATION.THEONEPROBE.TOP_INTEGRATION) {
+                        ITOPEntityInfoProvider provider = (ITOPEntityInfoProvider) entity;
+                        provider.addProbeInfo(mode, probeInfo, player, world, entity, data);
                     }
                 }
             });
